82b707ee9f36dff82c452bfaae195e6cd5625a5b
Simon Schubert <sschubert89@gmail.com>
2019-03-25 00:33:53 +0100

    Finalize release 1.0

--- shared/src/commonMain/kotlin/com/inspiredandroid/newsout/models/ExploreFeed.kt
+++ shared/src/commonMain/kotlin/com/inspiredandroid/newsout/models/ExploreFeed.kt
@@ -14,14 +14,16 @@ data class ExploreFeed(
     companion object {
         val exploreFeeds = listOf(
             ExploreFeed("Nextcloud", "https://nextcloud.com/blogfeed"),
-            ExploreFeed("Opensource.com", "https://opensource.com/feed"),
-            ExploreFeed("Electrek", "https://electrek.co/feed/"),
-            ExploreFeed("Bitwarden", "https://blog.bitwarden.com/"),
             ExploreFeed("Hacker News", "https://news.ycombinator.com/rss"),
-            ExploreFeed("Purism", "https://puri.sm/feed/"),
-            ExploreFeed("Mycroft AI", "https://mycroft.ai/blog/category/news/"),
-            ExploreFeed("Steam", "https://store.steampowered.com/feeds/news.xml"),
+            ExploreFeed("Opensource.com", "https://opensource.com/feed"),
+            ExploreFeed("Mycroft AI", "https://mycroft.ai/feed/"),
             ExploreFeed("Mozilla", "https://blog.mozilla.org/feed/"),
+            ExploreFeed("Electrek", "https://electrek.co/feed/"),
+            ExploreFeed("TechCrunch", "https://techcrunch.com/feed/"),
+            ExploreFeed("SpaceX", "https://www.spacex.com/news.xml"),
+            ExploreFeed("Bitwarden", "https://blog.bitwarden.com/feed"),
+            ExploreFeed("Purism", "https://puri.sm/feed/"),
+            ExploreFeed("Steam", "https://store.steampowered.com/feeds/news.xml"),
             ExploreFeed("Joe Rogan Podcast", "http://podcasts.joerogan.net/feed")
         )
     }
