70e8be16148377cf70d46b082d6280c651172bf0
Simon Schubert <sschubert89@gmail.com>
2019-03-31 21:13:07 +0200

    Add password reset link

--- app/src/main/java/com/inspiredandroid/newsout/activities/LoginActivity.kt
+++ app/src/main/java/com/inspiredandroid/newsout/activities/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.inspiredandroid.newsout.activities
 import android.accounts.Account
 import android.accounts.AccountManager
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
@@ -51,6 +52,11 @@ class LoginActivity : AppCompatActivity() {
             val dialog = InfoDialog.getInstance()
             dialog.show(supportFragmentManager, "TAG")
         }
+        tvResetPassword.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://nx3217.your-next.cloud"))
+            startActivity(browserIntent)
+        }
+
         radioGroup.setOnCheckedChangeListener { _, checkedId ->
             mode = if (checkedId == R.id.nextcloudBtn) {
                 MODE_CUSTOM
