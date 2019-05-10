165054b13a36fdaee8c3c3a30387020703c284cb
Simon Schubert <sschubert89@gmail.com>
2019-05-09 20:07:31 -0700

    Add eap to update checker exception

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
     resolutionStrategy {
         componentSelection {
             all {
-                val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview")
+                val rejected = listOf("eap", "alpha", "beta", "rc", "cr", "m", "preview")
                     .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
                     .any { it.matches(candidate.version) }
                 if (rejected) {
