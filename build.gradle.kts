80d299e043d8f75d67247207db1036871b50bcad
Simon Schubert <sschubert89@gmail.com>
2019-05-09 19:09:29 -0700

    Convert settings and project gradle to kts

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+buildscript {
+    val kotlin_version = "1.3.31"
+    val sqldelight_version = "1.1.3"
+
+    repositories {
+        google()
+        maven ("https://plugins.gradle.org/m2/")
+        maven ("https://dl.bintray.com/kotlin/kotlin-eap" )
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlin_version")
+        classpath("gradle.plugin.com.wiredforcode:gradle-spawn-plugin:0.8.2")
+        classpath("com.squareup.sqldelight:gradle-plugin:$sqldelight_version")
+    }
+}
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.21.0"
+}
+
+allprojects {
+    repositories {
+        maven ( "https://dl.bintray.com/kotlin/kotlinx" )
+        maven ( "https://dl.bintray.com/kotlin/ktor" )
+        maven ( "https://dl.bintray.com/soywiz/soywiz" )
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview")
+                    .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
+                    .any { it.matches(candidate.version) }
+                if (rejected) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
+    // optional parameters
+    checkForGradleUpdate = true
+}
