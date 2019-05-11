fbf36ecd89714cb149e329ab96a5c00f22a1056d
Kevin Galligan <kgalligan@gmail.com>
2019-05-11 16:52:22 -0400

    Update sync version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 buildscript {
     val kotlin_version = "1.3.31"
     val sqldelight_version = "1.1.3"
-    val xcodesync_version = "0.1.4"
+    val xcodesync_version = "0.1.5"
 
     repositories {
         google()
@@ -11,10 +11,6 @@ buildscript {
         maven ("https://dl.bintray.com/kotlin/kotlin-eap" )
         jcenter()
         mavenCentral()
-        maven {
-            setUrl("file:///Users/kgalligan/devel_kmp/kotlin-xcode-sync/build/localMaven")
-        }
-
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.0")
