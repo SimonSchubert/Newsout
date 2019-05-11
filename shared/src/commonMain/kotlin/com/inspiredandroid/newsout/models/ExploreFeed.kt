d9b75f90aca1b3fad72db7c32b59019874e6bceb
Simon Schubert <sschubert89@gmail.com>
2019-05-10 22:32:20 -0700

    Add more explore feeds

--- shared/src/commonMain/kotlin/com/inspiredandroid/newsout/models/ExploreFeed.kt
+++ shared/src/commonMain/kotlin/com/inspiredandroid/newsout/models/ExploreFeed.kt
@@ -13,23 +13,25 @@ data class ExploreFeed(
 ) {
     companion object {
         val exploreFeeds = listOf(
-            ExploreFeed("Nextcloud", "https://nextcloud.com/blogfeed"),
             ExploreFeed("Hacker News", "https://news.ycombinator.com/rss"),
+            ExploreFeed("TechCrunch", "https://techcrunch.com/feed"),
             ExploreFeed("Opensource.com", "https://opensource.com/feed"),
-            ExploreFeed("Mycroft AI", "https://mycroft.ai/feed/"),
-            ExploreFeed("Mozilla", "https://blog.mozilla.org/feed/"),
-            ExploreFeed("Electrek", "https://electrek.co/feed/"),
-            ExploreFeed("OpenAI", "https://openai.com/blog/rss/"),
-            ExploreFeed("TechCrunch", "https://techcrunch.com/feed/"),
+            ExploreFeed("Electrek", "https://electrek.co/feed"),
+            ExploreFeed("OpenAI", "https://openai.com/blog/rss"),
             ExploreFeed("SpaceX", "https://www.spacex.com/news.xml"),
-            ExploreFeed("Nasa", "https://www.nasa.gov/rss/dyn/breaking_news.rss"),
+            ExploreFeed("NASA", "https://www.nasa.gov/rss/dyn/breaking_news.rss"),
+            ExploreFeed("Nextcloud", "https://nextcloud.com/blogfeed"),
             ExploreFeed("Bitwarden", "https://blog.bitwarden.com/feed"),
-            ExploreFeed("Purism", "https://puri.sm/feed/"),
+            ExploreFeed("Mycroft AI", "https://mycroft.ai/feed"),
+            ExploreFeed("Mozilla", "https://blog.mozilla.org/feed"),
+            ExploreFeed("Purism", "https://puri.sm/feed"),
+            ExploreFeed("EurekAlert!", "https://www.eurekalert.org/rss.xml"),
             ExploreFeed("Steam", "https://store.steampowered.com/feeds/news.xml"),
             ExploreFeed("Github", "https://github.blog/feed/atom"),
             ExploreFeed("Kotlin language", "https://blog.jetbrains.com/kotlin/feed"),
             ExploreFeed("Rust language", "https://blog.rust-lang.org/feed.xml"),
             ExploreFeed("Swift language", "https://swift.org/atom.xml?format=xml"),
+            ExploreFeed("Inhabitat", "https://inhabitat.com/feed"),
             ExploreFeed("Joe Rogan Podcast", "http://podcasts.joerogan.net/feed")
         )
     }
